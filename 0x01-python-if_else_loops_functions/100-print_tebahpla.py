9a0ae13b905c7a00dedec25e320bb6e57eba981e
TuyisengeEdgar <tuyisengeedgar@gmail.com>
2022-08-23 22:08:19 +0000

    task 15

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
  #!/usr/bin/python3
 
- i=0
- for c in range(ord('z'), ord('a')-1, -1):
-     print("{}".format(chr(c - i)), end="")
-     i = 32 if i == 0 else 0
+ for char in range(ord('z'), ord('a') - 1, -1):
+     if char % 2 is 1:
+         char = char - ord(' ')
+     print("{}".format(chr(char - i)), end="")
+     
